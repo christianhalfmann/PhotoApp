4f387832a9599e7b0c713b8008fd8fa66bda47e0
Andreas <dev.athyrion@gmail.com>
2015-06-28 14:50:22 +0200

    little changes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res, next) {
     res.sendFile('index.html');
 });
 
+/* POST request to index.html */
+router.post('/', function(req, res, next) {
+    // TODO get session json
+    res.json(req.body.sessionID);
+});
+
 /* GET start page. */
 router.get('/start', function(req, res, next) {
     res.sendFile('./start.html', {
