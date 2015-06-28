9cc03593687c445b223c1732f99067b553ea38c9
Andreas <dev.athyrion@gmail.com>
2015-06-28 14:16:25 +0200

    deleted bin/www changed package.json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,10 +9,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
+var port = 60127;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -30,29 +32,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+var server = http.createServer(app);
+server.listen(port, function() {
+    console.log('> Server listening on port: ' + port);
 });
-
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile('index.html');
+});
+
+/* GET start page. */
+router.get('/start', function(req, res, next) {
+    res.sendFile('./start.html', {
+        root: 'public'
+    });
 });
 
 module.exports = router;
