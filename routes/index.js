bc287bd84717394082b458b97fdc336173e5d5a3
Andreas <dev.athyrion@gmail.com>
2015-07-02 22:00:53 +0200

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,4 @@ router.get('/', function(req, res, next) {
     res.sendFile('index.html');
 });
 
-/* POST request to index.html */
-router.post('/', function(req, res, next) {
-    // TODO get session json
-    var id = req.body.sessionID;
-    var dummyData = {
-        lastname: 'Halfmann',
-        firstname: 'Christian'
-    };
-
-    res.json(dummyData);
-});
-
-/* GET start page. */
-router.get('/start', function(req, res, next) {
-    res.sendFile('./start.html', {
-        root: 'public'
-    });
-});
-
 module.exports = router;
